e76dcb0b7193419f4a66028416cc544b2cffc37d
Tony Hallam <trhallam@gmail.com>
2022-03-02 09:15:26 +0000

    add missing commits

--- src/digirock/datasets.py
+++ src/digirock/datasets.py
@@ -7,15 +7,15 @@ from . import __local_version__
 
 GOODBOY = pooch.create(
     path=os.curdir,
-    base_url="https://raw.githubusercontent.com/trhallam/digirock/{version}/tests/test_data/",
+    base_url="https://github.com/trhallam/digirock/raw/{version}/tests/test_data/",
     version=__local_version__,
     # If this is a development version, get the data from the master branch
     version_dev="main",
     # The registry specifies the files that can be fetched from the local storage
     registry={
-        "COMPLEX_PVT.inc": "md5:492cb074b8232283b02e18a18a7777ec",
-        "PVT_BO.inc": "md5:1582ab00bc170cecd21bdfd5d3221f81",
-        "PVT_RS.inc": "md5:aa882bc90020b529d73d346767e46481",
+        "COMPLEX_PVT.inc": "3018c7ec33dded551e0bcd44103a1abd27ff4895268c712197616e396532da25",
+        "PVT_BO.inc": "053669c122948b690b03bcd2e5d11bdbc377bf84cddcd0d614ee19ec22ca36b6",
+        "PVT_RS.inc": "ff869731b2ece69fa0686b6a0204f113a0106e359413ddf1547841cbdf3d219d",
     },
 )
 
