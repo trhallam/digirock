bf20ba5d40218b5f653a2ba3b362a345199668b9
Tony Hallam <trhallam@gmail.com>
2022-03-15 17:17:37 +0000

    add PropsDict as custom type

--- src/digirock/typing.py
+++ src/digirock/typing.py
@@ -1,7 +1,8 @@
-from typing import Union
+from typing import Union, Dict
 from numpy import ndarray
 import pathlib
 
 NDArrayOrFloat = Union[ndarray, float]
 NDArrayOrInt = Union[ndarray, int]
 Pathlike = Union[str, pathlib.Path]
+PropsDict = Dict[str, Union[NDArrayOrFloat, NDArrayOrInt]]
